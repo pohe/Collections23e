e79a51a041813519cff58723e4fb05ab08ea16e0
Poul Henriksen <POHE@zealand.dk>
2023-10-06 10:27:37 +0200

    Add project files.

---
+++ Collections23e/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections23e
+{
+    public class Player
+    {
+		private int myVar;
+
+		public int MyProperty
+		{
+			get { return myVar; }
+			set { myVar = value; }
+		}
+
+
+	}
+}
---
+++ Collections23e/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+List<int> myList = new List<int>();
+myList.Add(10);
+myList.Add(20);
+myList.Add(30);
+myList.Insert(1, 15);
+myList[1] = 32;
+myList.Remove(20);
+myList.Insert(0, myList[2]);
+
+myList.Sort();
+
+foreach (var item in myList)
+{
+    Console.Write(item + " ");
+}
+
+
+
+
